a52ef6ff5ae5bb0ce7f0209a62f3d4c4d0408e40
andrescn2020 <andrescn_2010@hotmail.com>
2022-08-01 09:00:37 -0300

    New CV

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -14,7 +14,7 @@ const Hero = (props) => (
       <SectionText>
         Hello World!!!
       </SectionText>
-      <Button target="_blank" onClick={() => window.location = "https://www.dropbox.com/s/2d3c31qlwvz1khk/CapanoAndres-CV-Desarrollador-de-Software.pdf?dl=0"}>CV</Button>
+      <Button target="_blank" onClick={() => window.location = "https://www.dropbox.com/s/c2rb8zt3jzjqhex/CapanoAndres-CV-Desarrollador-de-Software.pdf?dl=0"}>CV</Button>
     </LeftSection>
   </Section>
 );
